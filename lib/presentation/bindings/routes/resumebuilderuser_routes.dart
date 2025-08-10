d3419e482cebcca761b9ac4e2df2c02abaef1c3e
vaso.vucinic <vasoct@gmail.com>
2025-08-10 12:12:53 +0200

    app routes service added

---
+++ lib/presentation/bindings/routes/resumebuilderuser_routes.dart
@@ -0,0 +1,10 @@
+import 'package:wtoolboxweb/router/wtw_routes.dart';
+import '../../view/home/home_view.dart';
+
+class ResumeBuilderUserRoutes extends WTWRoutes{
+
+  ResumeBuilderUserRoutes() {
+    registerRoute(name: '/', view: () => HomeView());
+  }
+
+}
